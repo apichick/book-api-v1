0d1aad95da34753add4b89221fd0838536a6c56e
mirene <mesnaola@apigee.com>
2018-05-07 12:41:26 +0100

    generalized pom, so it is also usable if there is node.js target, if the API security is basic authentication of client credentials and if the user defines other test.* properties, so they are passed to the integration tests too

--- apiproxy/resources/node/index.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-console.log('node.js application starting...');
-var svr = http.createServer(function(req, resp) {
-    resp.end('Hello, Node!');
-});
-
-svr.listen(process.env.PORT || 9000, function() {
-    console.log('Node HTTP server is listening');
-});
