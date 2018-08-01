636bd6a362e3a0995e2f8a5044809ce1d327dff4
mirene <mesnaola@apigee.com>
2018-08-01 19:10:25 +0100

    Added smartdocs update

--- update_smartdocs.sh
+++ update_smartdocs.sh
@@ -7,9 +7,7 @@ fi
 USERNAME=$1
 PASSWORD=$2
 
-RESULT=$(curl -s -o /dev/null -I -w "%{http_code}" -u ${USERNAME}:${PASSWORD} -H "Accept: application/json" http://dev-ylesyuk.devportal.apigee.io/smartdocs/apis/models/book-api-v1)
-
-echo $RESULT
+RESULT=$(curl -s -o /dev/null -w "%{http_code}" -u ${USERNAME}:${PASSWORD} -H "Accept: application/json" http://dev-ylesyuk.devportal.apigee.io/smartdocs/apis/models/book-api-v1)
 
 if [ $RESULT -eq 404 ]; then
     curl -v -X POST -u ${USERNAME}:${PASSWORD} -H "Content-Type: multipart/form-data;" -F "description=Book API v1" -F "name=book-api-v1" -F "display_name=Book API v1" "http://dev-ylesyuk.devportal.apigee.io/smartdocs/apis/models"
@@ -17,10 +15,6 @@ fi
 
 curl -v -X POST -u ${USERNAME}:${PASSWORD} -H "Content-Type: multipart/form-data;" -F "api_definition=@book-api-v1-spec.json" "http://dev-ylesyuk.devportal.apigee.io/smartdocs/apis/models/book-api-v1/import"
 
-echo "before render..."
-
 curl -X POST -u ${USERNAME}:${PASSWORD} "http://dev-ylesyuk.devportal.apigee.io/smartdocs/apis/models/book-api-v1/render"
-
-echo "after render..."
 
 curl -v "http://dev-ylesyuk.devportal.apigee.io/cron.php?cron_key=Yg9UaqudC8-DS0yuCUR0PTU70zKDE--mIC3E8Ft_lDQ"
